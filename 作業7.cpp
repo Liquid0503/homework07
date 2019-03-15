c540a006d542f894c25d1fd3b8329448450ee342
Liquid0503 <48340439+Liquid0503@users.noreply.github.com>
2019-03-15 11:53:25 +0800

    Add files via upload

---
+++ 作業7.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int n,i=1,su=0;
+	do
+	{
+		printf("秀出1+2+....+n的總和:");
+		scanf("%d",&n);
+	}
+	while (n<=0);
+	do 
+	su+=i++;
+	while (i <= n);
+	printf("1+2+...+%d=%d\n",n,su);
+	system("pause");
+	return 0;
+	
+}
